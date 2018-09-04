fb0ad6c7adfbeab5ea984701c78bec29e767ed87
Averi Johnson <averimj@live.com>
2018-09-04 15:09:35 -0600

    Add player test

--- lib/player.rb
+++ lib/player.rb
@@ -1,6 +1,27 @@
 class Player
+  attr_reader :board_grid
 
-  def initialize 
-    @board = Board.new
+
+  def initialize(move, board_grid)
+    @move = move
+    @board_grid = board_grid
+    @move_index = nil
   end
+
+  def move
+    @move.upcase
+  end
+
+  def find_move(grid)
+    grid.each_with_index do |
+  end
+
+  # def player_turn(move)
+  #
+  # end
+  #
+  # def store_move(grid)
+  #
+  # end
+
 end
--- test/player_test.rb
+++ test/player_test.rb
@@ -5,27 +5,28 @@ require './lib/player'
 
 class PlayerTest < Minitest::Test
   def test_it_exists
-    player_1 = Player.new(move, board)
+    board = Board.new
+    player_1 = Player.new("A3", board)
 
     assert_instance_of Player, player_1
   end
 
-  def test_it_has_attributes
-    player_1 = Player.new(two_unit_ship, three_unit_ship, board)
+  # def test_it_has_attributes
+  #   player_1 = Player.new(, board)
+  #
+  #   assert_equal board, player.board
+  # end
 
-    assert_equal , player.
-  end
-
-  def test_it_can_place_ships
-    player_1 = Player.new(two_unit_ship, three_unit_ship, board)
-
-    assert_equal two_unit_ship, player_1.ship_placement
-    assert_equal three_unit_ship, player_1.ship_placement
-  end
-  def test_it_can_store_players_move
-    player_1 = Player.new
-
-    assert_equal "B3", player_1.store_move(@board.grid)
-  end
+  # def test_it_can_place_ships
+  #   player_1 = Player.new(two_unit_ship, three_unit_ship, board)
+  #
+  #   assert_equal ?, player_1.ship_placement
+  # end
+  #
+  # def test_it_can_store_players_move
+  #   player_1 = Player.new
+  #
+  #   assert_equal "B3", player_1.store_move(@board.grid)
+  # end
 
 end
